6c5b54bed19c25ce2e27dc3195ca42f0c08cec41
ms5932867 <79813095+ms5932867@users.noreply.github.com>
2026-05-06 20:28:13 -0700

    Add: n-queens - submission-2

---
+++ Data Structures & Algorithms/n-queens/submission-2.java
@@ -0,0 +1,68 @@
+class Solution {
+    List<List<String>> res = new ArrayList<>();
+    public List<List<String>> solveNQueens(int n) {
+        // boolean[][] used = new boolean[n][n];
+        boolean[][] queen = new boolean[n][n];
+        dfs(queen, n, 0);
+        return res;
+    }
+
+    private void dfs(boolean[][] queen, int n, int i) {
+        if (i == n) {
+            List<String> cur = convert(queen);
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        //i is row and j is col
+        for (int j = 0; j < n; j++) {
+            if (isValid(queen, n, i, j)) {
+                queen[i][j] = true;
+                dfs(queen, n, i + 1);
+                queen[i][j] = false;
+            }
+        }
+    }
+    private List<String> convert(boolean[][] queen) {
+        List<String> cur = new ArrayList<>();
+        for (int i = 0; i < queen.length; i++) {
+            StringBuilder curRow = new StringBuilder();
+            for (int j = 0; j < queen[0].length; j++) {
+                if (queen[i][j]) {
+                    curRow.append("Q");
+                } else {
+                    curRow.append(".");
+                }
+            }
+            cur.add(curRow.toString());
+
+        }
+        return cur;
+    }
+    private boolean isValid(boolean[][] queen, int n, int i, int j) {
+        // same row on the left
+        for (int col = 0; col <= j; col++) {
+            if (queen[i][col]) {
+                return false;
+            }
+        }
+        // same col on the left
+        for (int row = 0; row <= i; row++) {
+            if (queen[row][j]) {
+                return false;
+            }
+        }
+        // same diagonal on the left
+        for (int row = i, col = j; row >= 0 && col >= 0; row--, col--) {
+            if (queen[row][col]) {
+                return false;
+            }
+        }
+        for (int row = i, col = j; row >= 0 && col < n; row--, col++) {
+            if (queen[row][col]) {
+                return false;
+        }
+}
+        return true;
+    }
+
+}
